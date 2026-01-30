67f951aab9786478a094dcf72cd5c9aff215ece0
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-01-30 18:13:00 +0530

    Update Que8.c

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que8.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que8.c
@@ -1,6 +1,8 @@
+//perimeter of rectangle
 void main(){
     int length = 100;
     int breadth = 50;
-    int perimeter = 2*(length+breadth);
+    int perimeter = 2 * (length + breadth);
     printf("perimeter of rectangle is %d",perimeter); 
+
 }
