7a39f36511ef44fc55f047693a39aabb96caf846
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-01-30 18:13:24 +0530

    Update Que9.c

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que9.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que9.c
@@ -1,6 +1,8 @@
+//Area of rectangle
 void main(){
     int base = 10;
     int height = 20;
-    int area = base * height /2;
+    int area = base * height / 2;
     printf("Area of rectangle is %d",area);
+
 }
