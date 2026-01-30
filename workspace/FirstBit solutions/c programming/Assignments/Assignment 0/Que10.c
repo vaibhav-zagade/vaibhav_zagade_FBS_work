1eb558f023fbeff71623d9e677d4213fea3dd8a1
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-01-30 18:08:15 +0530

    Update Que10.c

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que10.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que10.c
@@ -1,7 +1,9 @@
+//total marks and perrcentage of five subjects
 void main(){
     int total_marks = 500;
     int physics = 85, chemistry = 87, math = 83, biology = 90, bhoogol = 99;
     int marks_obt = physics + chemistry + math + biology + bhoogol;
     float percentage = (marks_obt*100.0)/total_marks;
     printf("Total marks of 5 subjects is %d and percentage is %f",marks_obt,percentage);
+
 }
