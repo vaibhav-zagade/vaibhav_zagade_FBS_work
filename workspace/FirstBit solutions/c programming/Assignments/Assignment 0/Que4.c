c43ccf849f640803a4de362e0f7b457b8cba9825
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-01-30 18:10:18 +0530

    Update Que4.c

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que4.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que4.c
@@ -1,3 +1,4 @@
+//Swapping of numbers
 void main(){
     int num1 = 10,num2 = 20;
     int tempNum;
@@ -5,5 +6,6 @@ void main(){
     num1 = num2;
     num2 = tempNum;
     printf("After swapping first number is %d and second number is %d",num1,num2);
+
 
 }
