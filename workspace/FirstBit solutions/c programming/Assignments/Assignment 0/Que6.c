f78b7b19fd16f588437fd1e70a7f95d3d2d142e7
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-01-30 18:11:49 +0530

    Update Que6.c

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que6.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que6.c
@@ -1,6 +1,8 @@
+//finding square and cube of numbers
 void main(){
     int num = 6;
     int square = num * num;
     int cube = square * num;
     printf("square of a number is %d and cube of a given number is %d",square,cube);
+
 }
