9bde413e9a96304c642bb6f08d339bfd6b532343
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-01-30 18:12:26 +0530

    Update Que7.c

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que7.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que7.c
@@ -1,7 +1,9 @@
+//conversion of minutes in hours and minutes
 void main(){
     int totalMinutes = 150;
     int hours, minutes;
     hours = totalMinutes/60;
     minutes = totalMinutes%60;
     printf("%d minutes = %d hours & %d minutes ",totalMinutes,hours,minutes);
+
 }
