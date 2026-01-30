48240dce7fbe78f65818c335843ffea53a464f13
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-01-30 18:10:53 +0530

    Update Que5.c

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que5.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 0/Que5.c
@@ -1,7 +1,9 @@
+//average of five numbers
 void main(){
     int num1 = 10, num2 = 20, num3 = 30, num4 = 40, num5 = 50;
     float average;
     average = (num1+num2+num3+num4+num5)/5.0;
     printf("the average of 5 numbers is %f",average);
+
 
 }
