3c533f5826ccb33a3060f6dacd482cd67802a472
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-02-06 09:49:16 +0530

    Update Que7.c

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 1/Que7.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 1/Que7.c
@@ -1,21 +1,21 @@
-//calculating salary based on basic
+/* Calculating total salary based on basic. If basic <=5000 da, ta and hra will be
+10%,20% and 25% respectively otherwise da, ta and hra will be 15%,25% and 30%
+respectively.*/
 
 void main(){
     double basic = 6000,da,ta,hra,total_sal;
     if(basic <= 5000){
-        da = (basic*10)/100;
-        ta = (basic*20)/100;
-        hra =  (basic*25)/100;
-        total_sal = basic + da + ta + hra;
-        printf("Total salary is %d (with da = %d, ta = %d, hra = %d)",total_sal,da,ta,hra);
+        da = basic*0.1;
+        ta = basic*0.2;
+        hra =  basic*0.25;  
     }
     else{
-        da = (basic*15)/100;
-        ta = (basic*25)/100;
-        hra =  (basic*30)/100;
-        total_sal = basic + da + ta + hra;
-        printf("Total salary is %d (with da = %d, ta = %d, hra = %d)",total_sal,da,ta,hra);
+        da = basic*0.15;
+        ta = basic*0.25;
+        hra =  basic*0.3;
     }
+    total_sal = basic + da + ta + hra;
+    printf("Total salary is %.2f (with da = %.2f, ta = %.2f, hra = %.2f)",total_sal,da,ta,hra);
 
 
 }
