7e84bb485a14a4a311915759b5a294791f2ab797
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-02-07 12:00:15 +0530

    Simplify palindrome check for 3-digit number

--- workspace/FirstBit solutions/c programming/Assignments/Assignment 1/Que2.c
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 1/Que2.c
@@ -1,17 +1,11 @@
 //checking 3 digit number is palindrome or not.
 void main(){
-    int num = 121,a,b,c,q1,rev_num;
-    a = num % 10;
-    q1 = num / 10;
-    b = q1 % 10;
-    c = q1 / 10;
-    rev_num = a*100 + b*10 + c;
+    int num = 121;
     
-    if(num == rev_num){
+    if(num %10 == num/100){
         printf("%d is a palindrome",num);
     }
     else{
         printf("%d is not a palindrome",num);
     }
-    
 }
