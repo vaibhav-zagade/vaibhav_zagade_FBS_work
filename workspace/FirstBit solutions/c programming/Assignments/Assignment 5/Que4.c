3157d61b4a6ef56d4b0d5a82cc50b3c6bf24b6de
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-02-15 14:33:14 +0530

    pyramid pattern printing program
    Implement pyramid pattern printing in C.

---
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 5/Que4.c
@@ -0,0 +1,23 @@
+// pyramid pattern
+// Input: n = 5
+// Output:
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+
+
+
+void main(){
+    int n = 5;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n-i; j++){
+            printf(" ");
+        }
+        for(int j = 1; j <= i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
