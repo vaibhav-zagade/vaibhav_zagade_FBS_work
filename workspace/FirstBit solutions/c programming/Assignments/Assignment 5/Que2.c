4f3a4bad3f8ccfc01e53d98a6052dfce4d621181
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-02-15 14:19:20 +0530

    right angled triangle pattern printing program
    Implement a program to print a right-angled triangle pattern of asterisks based on user-defined height.

---
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 5/Que2.c
@@ -0,0 +1,19 @@
+// Print a right-angled triangle pattern
+// Input: n = 5
+// Output:
+// *
+// **
+// ***
+// ****
+// *****
+
+void main(){
+    int n = 10;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
