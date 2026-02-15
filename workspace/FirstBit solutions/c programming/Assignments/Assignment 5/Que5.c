91d2ec936ef57de92f737230c0df33ac0e871284
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-02-15 14:46:47 +0530

    Inverted pyramid pattern printing program

---
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 5/Que5.c
@@ -0,0 +1,24 @@
+// Print an inverted pyramid pattern
+// Input: n = 5
+// Output:
+
+// * * * * *
+//  * * * *
+//   * * *
+//    * *
+//     *
+
+
+void main(){
+    int n = 10;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i ; j++){
+            printf(" ");
+        }
+        for(int j = n - i; j > 0 ; j--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
