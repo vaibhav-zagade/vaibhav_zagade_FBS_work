a7e39e1d29e763f99f959c61ada2a8723eb08323
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-02-15 13:47:31 +0530

    solid square pattern printing program
    Implement a program to print a solid square pattern of asterisks.

---
+++ workspace/FirstBit solutions/c programming/Assignments/Assignment 5/Que1.c
@@ -0,0 +1,21 @@
+// 1. Print a solid square pattern
+// Input: n = 4
+// Output:
+
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+void main(){
+    int n = 4;
+
+    for(int row = 1; row<= n; row++){
+        for(int col = 1; col <= n; col++ ){
+            printf("* ");
+        }
+        printf("\n");
+
+    }
+    
+}
