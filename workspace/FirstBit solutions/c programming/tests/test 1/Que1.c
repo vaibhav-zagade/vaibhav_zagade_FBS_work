1af493e2e7fc37bc749675e14591e7242844bcdc
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-03-28 10:55:01 +0530

    Que1.c

--- workspace/FirstBit solutions/c programming/tests/test 1/Que1.c
+++ workspace/FirstBit solutions/c programming/tests/test 1/Que1.c
@@ -3,12 +3,14 @@
 
 void main(){
     printf("Calculate sum of prime numbers in a given range.\n");
-    printf("Enter a number : ");
-    int num;
+    printf("Enter a starting number : ");
+    int num1,num2;
     int sum=0;
-    scanf("%d",&num);
+    scanf("%d",&num1);
+    printf("Enter a ending number : ");
+    scanf("%d",&num2);
 
-    for(int i = 2; i<=num; i++){
+    for(int i = num1; i<=num2; i++){
         int flag = 1;
         for(int j = 2; j<=i/2; j++){
             if(i%j==0){
@@ -20,5 +22,5 @@ void main(){
             sum = sum + i;
         }
     }
-    printf("Sum of prime numbers upto %d : %d",num,sum);
+    printf("Sum of prime numbers in range %d to %d : %d",num1,num2,sum);
 }
