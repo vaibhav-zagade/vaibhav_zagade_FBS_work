a45f8f4c745f064cd42a27ecda68649af865137f
Zagade Vaibhav <zagadevaibhav50@gmail.com>
2026-03-29 18:49:40 +0530

    calculate square and cube of a number

---
+++ workspace/FirstBit solutions/Java programming/Assignments/Assignment 1/Que6.java
@@ -0,0 +1,10 @@
+package assignment_0;
+
+public class Que6 {
+	public static void main(String args[]){
+	    int num = 6;
+	    int square = num * num;
+	    int cube = square * num;
+	    System.out.printf("square of a number is %d and cube of a given number is %d",square,cube);
+	}
+}
